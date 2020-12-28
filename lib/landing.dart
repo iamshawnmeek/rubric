826a2aa15dc0e301548967b8c41d8695411a3283
iamshawnmeek <shawn@shawnmeek.com>
2020-12-28 10:29:54 -0700

    Formatting logo to Landing

--- lib/landing.dart
+++ lib/landing.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:rubric/colors.dart';
 
 class Landing extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SafeArea(
+              child: SvgPicture.asset(
+                'assets/images/logo.svg',
+                color: primary,
+                semanticsLabel: 'rubric logo',
+                width: 295,
+                height: 89,
+              ),
+            ),
+            SizedBox(height: 300), //is this the best way to do this?
+          ],
+        ),
+      ),
+    );
   }
 }
