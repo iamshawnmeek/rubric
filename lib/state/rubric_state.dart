4543c6f8a6d521573ca06c882fcb5cd683a1f9cb
iamshawnmeek <shawn@shawnmeek.com>
2021-01-11 07:08:57 -0700

    wip: updates to accept dynamic entries

--- lib/state/rubric_state.dart
+++ lib/state/rubric_state.dart
@@ -2,4 +2,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rubric/domain/rubric.dart';
 
 final rubricProviderRef =
-    StateProvider<Rubric>((ref) => Rubric(objectives: []));
+    StateNotifierProvider<RubricState>((ref) => RubricState());
+
+//mechanism to update the Rubric class with the new objective
+class RubricState extends StateNotifier<Rubric> {
+  RubricState() : super(Rubric(objectives: []));
+
+//TODO: Add method to update objectives: 1.11.21
+}
