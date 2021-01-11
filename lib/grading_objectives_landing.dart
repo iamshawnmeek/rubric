0b9fc4f2b391e4c81b389b7442f84b2cb70671d7
iamshawnmeek <shawn@shawnmeek.com>
2021-01-11 07:06:49 -0700

    wip: updated to include riverpod

--- lib/grading_objectives_landing.dart
+++ lib/grading_objectives_landing.dart
@@ -3,11 +3,15 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:rubric/components/colors.dart';
 import 'package:rubric/components/create_card.dart';
 import 'package:rubric/components/rubric_card.dart';
+import 'package:rubric/state/rubric_state.dart';
 import 'package:rubric/typography/headline_one.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class GradingObjectivesLanding extends StatelessWidget {
+class GradingObjectivesLanding extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
+    final rubric = watch(rubricProviderRef.state);
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -20,6 +24,7 @@ class GradingObjectivesLanding extends StatelessWidget {
               SizedBox(height: 60),
               HeadlineOne('Grading Objectives'),
               SizedBox(height: 46),
+              //TODO: map over rubric objectives to create cards
               RubricCard(),
               SizedBox(height: 16),
               CreateCard(),
