35a84fb7705166c6fe31ca2204073802661a8b29
Jonah Walker <supposedlysam@gmail.com>
2021-01-12 06:26:04 -0700

    Add iterable extensions

---
+++ lib/iterable_extensions.dart
@@ -0,0 +1,38 @@
+extension IterableExtensions<T> on Iterable<T> {
+  /// Interlace the "separator" between each item.
+  ///
+  ///### Example 1: Strings
+  /// ```
+  /// final a = ['a', 'a', 'a'];
+  ///
+  /// a.joinWith('b'); -> ['a', 'b', 'a', 'b', 'a']
+  /// ```
+  ///
+  /// ### Example 2: Widgets
+  /// ```
+  /// final widgets = <Widget>[
+  ///   Text('item1'),
+  ///   Text('item2'),
+  ///   Text('item3'),
+  /// ];
+  ///
+  /// widgets.joinWith(Divider()); -> [
+  ///   Text('item1'),
+  ///   Divider(),
+  ///   Text('item2'),
+  ///   Divider(),
+  ///   Text('item3'),
+  /// ]
+  /// ```
+  List<T> joinWith<T>(T separator) {
+    if (this == null || separator == null) return null;
+    if (length <= 0) return [separator];
+    if (length == 1) return List<T>.from(this);
+
+    return List<T>.from(map((current) => [
+          current,
+          separator,
+        ]).expand((w) => w))
+      ..removeLast();
+  }
+}
