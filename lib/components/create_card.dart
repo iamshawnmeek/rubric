14d2185bf90f7cb8bdbf963a5a080f3e9faad8f1
iamshawnmeek <shawn@shawnmeek.com>
2021-01-13 06:56:29 -0700

    Worked to add OnPress functionality, shows error

--- lib/components/create_card.dart
+++ lib/components/create_card.dart
@@ -1,17 +1,31 @@
+import 'package:flow_builder/flow_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:rubric/components/colors.dart';
+import 'package:rubric/create_objective_bottom_sheet.dart';
+import 'package:rubric/enums.dart';
 
 class CreateCard extends StatelessWidget {
-  const CreateCard({
-    Key key,
-  }) : super(key: key);
+  final FlowController flowController; //updated 1.13.21
+
+  const CreateCard({Key key, @required this.flowController})
+      : super(key: key); //updated 1.13.21
 
   @override
   Widget build(BuildContext context) {
     return Opacity(
       opacity: .5,
       child: Container(
+        onPressed: () {
+          showModalBottomSheet(
+            isScrollControlled: true,
+            context: context,
+            builder: (context) => CreateObjectiveBottomSheet(
+              onCreatePressed: () => flowController
+                  .update((_) => OnboardingFlow.gradingObjectives),
+            ),
+          );
+        },
         height: 92,
         child: Center(
           child: FaIcon(FontAwesomeIcons.plus),
