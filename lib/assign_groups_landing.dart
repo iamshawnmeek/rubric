4e9e1e9604b6de682116627702e003ab30e3a00f
iamshawnmeek <shawn@shawnmeek.com>
2021-01-18 19:33:06 -0700

    Created assign groups landing, need build layout

---
+++ lib/assign_groups_landing.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AssignGroupsLanding extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
