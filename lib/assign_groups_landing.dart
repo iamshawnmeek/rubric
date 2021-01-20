2b829b677d5a1311758c9ce1815dc09c44ba1c1a
iamshawnmeek <shawn@shawnmeek.com>
2021-01-20 06:12:07 -0700

    Edited layout

--- lib/assign_groups_landing.dart
+++ lib/assign_groups_landing.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:rubric/typography/headline_one.dart';
+import 'components/small_logo.dart';
 
 class AssignGroupsLanding extends StatelessWidget {
   @override
@@ -6,7 +8,19 @@ class AssignGroupsLanding extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: SafeArea(
-          child: Container(),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(height: 36),
+                SmallLogo(),
+                SizedBox(height: 60),
+                HeadlineOne('Assign Groups'),
+                SizedBox(height: 46),
+              ],
+            ),
+          ),
         ),
       ),
     );
