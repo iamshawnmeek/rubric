ac91b0bc10ac4970b36ddecf838aa53a4f8a1971
iamshawnmeek <shawn@shawnmeek.com>
2021-01-20 07:03:53 -0700

    WIP: Adding stack and building out container

--- lib/assign_groups_landing.dart
+++ lib/assign_groups_landing.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rubric/components/colors.dart';
 import 'package:rubric/typography/headline_one.dart';
 import 'components/small_logo.dart';
 
@@ -6,22 +7,43 @@ class AssignGroupsLanding extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                SizedBox(height: 36),
-                SmallLogo(),
-                SizedBox(height: 60),
-                HeadlineOne('Assign Groups'),
-                SizedBox(height: 46),
-              ],
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    SizedBox(height: 36),
+                    SmallLogo(),
+                    SizedBox(height: 60),
+                    HeadlineOne('Assign Groups'),
+                    SizedBox(height: 46),
+                  ],
+                ),
+              ),
             ),
           ),
-        ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: FractionallySizedBox(
+                //fractions out the container
+                heightFactor: .5,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: primaryDark,
+                  ),
+                  height: 480, //to change later
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
