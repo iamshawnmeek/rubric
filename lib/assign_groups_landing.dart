d46774819c43eae0bfb66c0e58d7b1a478292a56
Jonah Walker <supposedlysam@gmail.com>
2021-01-22 07:10:29 -0700

    Add text field to assign groups and allow user to unfocus scope

--- lib/assign_groups_landing.dart
+++ lib/assign_groups_landing.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:rubric/components/colors.dart';
 import 'package:rubric/components/rubric_card.dart';
+import 'package:rubric/components/rubric_text_field.dart';
 import 'package:rubric/domain/rubric.dart';
 import 'package:rubric/state/rubric_state.dart';
 import 'package:rubric/typography/body_placeholder.dart';
@@ -17,81 +18,96 @@ class AssignGroupsLanding extends ConsumerWidget {
   Widget build(BuildContext context, ScopedReader watch) {
     final rubric = watch(rubricProviderRef.state);
 
-    return Scaffold(
-      body: Stack(
-        children: [
-          SingleChildScrollView(
-            child: SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 32),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    SizedBox(height: 36),
-                    SmallLogo(),
-                    SizedBox(height: 60),
-                    HeadlineOne('Assign Groups'),
-                    SizedBox(height: 46),
-                    DottedBorder(
-                      borderType: BorderType.RRect,
-                      dashPattern: [4, 4, 4, 4],
-                      color: lightGray,
-                      radius: Radius.circular(12),
-                      child: Container(
-                        height: 85,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Center(
-                          child: BodyPlaceholder(
-                            'Drag objective here',
-                            color: lightGray,
-                          ),
-                        ),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: Stack(
+          children: [
+            SingleChildScrollView(
+              child: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      SizedBox(height: 36),
+                      SmallLogo(),
+                      SizedBox(height: 60),
+                      HeadlineOne('Assign Groups'),
+                      SizedBox(height: 46),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          RubricTextField(
+                              onChanged: (_) {}, hintText: 'Group 1'),
+                          SizedBox(height: 24),
+                          _firstDragTarget(),
+                        ],
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: FractionallySizedBox(
-                heightFactor: .45,
-                child: Container(
-                  child: Padding(
-                    padding: const EdgeInsets.all(12),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        SizedBox(
-                          height: 52,
-                          width: 44,
-                          child: Align(
-                            alignment: Alignment.centerLeft,
-                            child: FaIcon(
-                              FontAwesomeIcons.chevronLeft,
-                              color: primaryLightest,
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: FractionallySizedBox(
+                  heightFactor: .45,
+                  child: Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          SizedBox(
+                            height: 52,
+                            width: 44,
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: FaIcon(
+                                FontAwesomeIcons.chevronLeft,
+                                color: primaryLightest,
+                              ),
                             ),
                           ),
-                        ),
-                        SizedBox(height: 24),
-                        ..._buildObjectives(rubric),
-                      ],
+                          SizedBox(height: 24),
+                          ..._buildObjectives(rubric),
+                        ],
+                      ),
                     ),
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: primaryDark,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: primaryDark,
+                    ),
                   ),
                 ),
               ),
             ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  DottedBorder _firstDragTarget() {
+    return DottedBorder(
+      borderType: BorderType.RRect,
+      dashPattern: [4, 4, 4, 4],
+      color: lightGray,
+      radius: Radius.circular(12),
+      child: Container(
+        height: 85,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Center(
+          child: BodyPlaceholder(
+            'Drag objective here',
+            color: lightGray,
           ),
-        ],
+        ),
       ),
     );
   }
