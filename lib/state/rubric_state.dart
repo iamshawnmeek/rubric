2fa1a86fc13b7bb51b9bcec04b2ff4833d427191
iamshawnmeek <shawn@shawnmeek.com>
2021-02-16 23:27:13 -0700

    WIP: Updates to RubricGroup and state.objectives

--- lib/state/rubric_state.dart
+++ lib/state/rubric_state.dart
@@ -13,7 +13,27 @@ class RubricState extends StateNotifier<Rubric> {
     state = state.copyWith(objectives: [...state.objectives, objective]);
   }
 
-  void addGroup(RubricGroup group) {
+  void addGroup({@required RubricGroup group, @required Objective objective}) {
+    //find the group that has the duplicate objective
+    final foundGroup = state.groups.firstWhere((group) {
+      return group.objectives.contains(objective);
+    });
+
+    //create copy of group with new objective list...SM: 2.16
+    final foundGroupCopy = state.groups.firstWhere((group) {
+      objectives:
+      [
+        ...state.objectives,
+        objective,
+      ];
+    });
+
+    // WIP Updated: 2.16.21: the types are off: RubricCopy versus Rubric, should be RubricGroup, state.copyWith should change
+    // WIP Updated: 2.16.21: reference lines 44 and 45 when updating this
+
+    //...remove objective from group that we've identified
+
+    //replace the current group with the new group
     state = state.copyWith(groups: [...state.groups, group]);
   }
 
