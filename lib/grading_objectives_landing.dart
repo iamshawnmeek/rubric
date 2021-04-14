1f9fd9cd72c6a57c03d137ad02aae944d9fb1945
Jonah Walker <supposedlysam@gmail.com>
2021-04-14 09:22:36 -0400

    Convert from using the ConsumerWidget to a StatefulWidget

--- lib/grading_objectives_landing.dart
+++ lib/grading_objectives_landing.dart
@@ -9,50 +9,73 @@ import 'package:rubric/components/rubric_card.dart';
 import 'package:rubric/components/small_logo.dart';
 import 'package:rubric/domain/rubric.dart';
 import 'package:rubric/enums.dart';
+import 'package:rubric/hooks/scroll_hook.dart';
 import 'package:rubric/iterable_extensions.dart';
 import 'package:rubric/list_extensions.dart';
 import 'package:rubric/state/rubric_state.dart';
 import 'package:rubric/typography/headline_one.dart';
 
-class GradingObjectivesLanding extends ConsumerWidget {
+class GradingObjectivesLanding extends StatefulWidget {
   final FlowController flowController;
 
   GradingObjectivesLanding({@required this.flowController});
 
   @override
-  Widget build(BuildContext context, ScopedReader watch) {
-    final rubric = watch(rubricProviderRef.state);
-    final shouldShowNextButton = rubric.objectives.length >= 2;
+  _GradingObjectivesLandingState createState() =>
+      _GradingObjectivesLandingState();
+}
 
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                SizedBox(height: 36),
-                SmallLogo(),
-                SizedBox(height: 60),
-                HeadlineOne('Grading Objectives'),
-                SizedBox(height: 46),
-                ..._buildObjectives(rubric),
-                SizedBox(height: 16),
-                CreateCard(),
-                SizedBox(height: 90),
-              ],
-            ),
+class _GradingObjectivesLandingState extends State<GradingObjectivesLanding> {
+  ScrollHookState scrollHook;
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (context, watch, _) {
+        final rubric = watch(rubricProviderRef.state);
+        final shouldShowNextButton = rubric.objectives.length >= 2;
+
+        return Scaffold(
+          body: ScrollHook(
+            builder: (context, scrollController, scrollHook) {
+              this.scrollHook = scrollHook;
+
+              return SingleChildScrollView(
+                controller: scrollController,
+                child: SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        SizedBox(height: 36),
+                        SmallLogo(),
+                        SizedBox(height: 60),
+                        HeadlineOne('Grading Objectives'),
+                        SizedBox(height: 46),
+                        ..._buildObjectives(rubric),
+                        SizedBox(height: 16),
+                        CreateCard(),
+                        SizedBox(height: 90),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
           ),
-        ),
-      ),
-      // Ternary Operator: basically an if/else statement
-      floatingActionButton: shouldShowNextButton
-          ? NextButton(onTap: () {
-              flowController.update((_) => OnboardingFlow.assignGroups);
-            })
-          : null,
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+          // Ternary Operator: basically an if/else statement
+          floatingActionButton: shouldShowNextButton
+              ? NextButton(onTap: () {
+                  widget.flowController.update(
+                    (_) => OnboardingFlow.assignGroups,
+                  );
+                })
+              : null,
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+        );
+      },
     );
   }
 
