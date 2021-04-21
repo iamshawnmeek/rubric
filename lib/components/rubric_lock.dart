d81184e8584d1287cded0d7acc515389a87a8ea3
iamshawnmeek <shawn@shawnmeek.com>
2021-04-21 07:06:32 -0600

    Updates to lock states default and active

--- lib/components/rubric_lock.dart
+++ lib/components/rubric_lock.dart
@@ -2,27 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 // import 'package:rubric/components/colors.dart';
 
-class RubricLock extends StatelessWidget {
+class RubricLock extends StatefulWidget {
   const RubricLock({
     Key key,
   }) : super(key: key);
 
   @override
+  _RubricLockState createState() => _RubricLockState();
+}
+
+class _RubricLockState extends State<RubricLock> {
+  bool isTapped = false;
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      behavior:
+          HitTestBehavior.opaque, //allows padding to be hit as well as icon
       onTap: () {
-        print('I have been tapped!');
+        setState(() {
+          isTapped =
+              !isTapped; //see how this aligns with line 15 and also line 25 logic
+        });
       },
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 11.5),
-        child: SvgPicture.asset(
-          'assets/images/icon-lock-default.svg',
-          // color: primary,
-          semanticsLabel: 'rubric lock default',
-          width: 16,
-          height: 21,
-        ),
+        child: isTapped ? buildActiveLock() : buildDefaultLock(),
       ),
+    );
+  }
+
+  Widget buildDefaultLock() {
+    return SvgPicture.asset(
+      'assets/images/icon-lock-default.svg',
+      // color: primary,
+      semanticsLabel: 'rubric lock default',
+      width: 16,
+      height: 21,
+    );
+  }
+
+  Widget buildActiveLock() {
+    return SvgPicture.asset(
+      'assets/images/icon-lock-locked.svg',
+      // color: primary,
+      semanticsLabel: 'rubric lock locked',
+      width: 16,
+      height: 20,
     );
   }
 }
