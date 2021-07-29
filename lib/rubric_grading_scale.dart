ebbdf3caf50e6e12a80324fa04f20fa9c62b3e3e
iamshawnmeek <shawn@shawnmeek.com>
2021-07-28 21:55:25 -0600

    wip: grading scale layout

--- lib/rubric_grading_scale.dart
+++ lib/rubric_grading_scale.dart
@@ -1,8 +1,11 @@
 import 'package:flow_builder/flow_builder.dart';
 import 'package:flutter/material.dart';
+import 'package:rubric/components/colors.dart';
 import 'package:rubric/components/setGradingScale_button.dart';
 import 'package:rubric/l10n/l10n.dart';
 import 'package:rubric/typography/body_headline.dart';
+import 'package:rubric/typography/toggle_btn_title.dart';
+import 'package:rubric/typography/toggle_btn_title_null.dart';
 
 class RubricGradingScale extends StatelessWidget {
   final FlowController flowController;
@@ -18,12 +21,40 @@ class RubricGradingScale extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 30),
-          child: Container(
-            child: BodyHeadline(l.gradingScaleTitle),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                child: BodyHeadline(l.gradingScaleTitle),
+              ),
+              SizedBox(height: 22),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Container(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 60, vertical: 12),
+                        child: ToggleBtnTitle(l.gradingScaleToggleSimple),
+                      ),
+                      decoration: BoxDecoration(
+                        color: primaryDark,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ToggleBtnTitleNull(l.gradingScaleToggleDetailed),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
       floatingActionButton: SetGradingScaleButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
