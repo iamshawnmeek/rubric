1a973124fb11f1927cb301226cebdeda023b1b15
iamshawnmeek <shawn@shawnmeek.com>
2021-08-02 06:35:43 -0600

    feat: added dynamic values for grading scale rows and inputs

--- lib/rubric_grading_scale.dart
+++ lib/rubric_grading_scale.dart
@@ -1,11 +1,12 @@
 import 'package:flow_builder/flow_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:rubric/components/colors.dart';
-import 'package:rubric/components/setGradingScale_button.dart';
+import 'package:rubric/components/set_grading_scale_button.dart';
 import 'package:rubric/l10n/l10n.dart';
 import 'package:rubric/typography/body_headline.dart';
-import 'package:rubric/typography/toggle_btn_title.dart';
-import 'package:rubric/typography/toggle_btn_title_null.dart';
+import 'package:rubric/typography/body_grading_scale_input.dart';
+import 'package:rubric/typography/toggle_button_title_active.dart';
+import 'package:rubric/typography/toggle_button_title_inactive.dart';
 
 class RubricGradingScale extends StatelessWidget {
   final FlowController flowController;
@@ -35,10 +36,11 @@ class RubricGradingScale extends StatelessWidget {
                     child: Container(
                       child: Padding(
                         padding: const EdgeInsets.symmetric(
-                            horizontal: 60,
-                            vertical:
-                                12), //note for JW: not sure if this horizontal / width is the right way to do this.
-                        child: ToggleBtnTitle(l.gradingScaleToggleSimple),
+                          horizontal: 60,
+                          vertical: 12,
+                        ), //note for JW: not sure if this horizontal / width is the right way to do this.
+                        child:
+                            ToggleButtonTitleActive(l.gradingScaleToggleSimple),
                       ),
                       decoration: BoxDecoration(
                         color: primaryDark,
@@ -50,10 +52,11 @@ class RubricGradingScale extends StatelessWidget {
                     child: Container(
                       child: Padding(
                         padding: const EdgeInsets.symmetric(
-                            horizontal: 50,
-                            vertical:
-                                12), //note for JW: not sure if this horizontal / width is the right way to do this.
-                        child: ToggleBtnTitleNull(l.gradingScaleToggleDetailed),
+                          horizontal: 50,
+                          vertical: 12,
+                        ), //note for JW: not sure if this horizontal / width is the right way to do this.
+                        child: ToggleButtonTitleInactive(
+                            l.gradingScaleToggleDetailed),
                       ),
                       decoration: BoxDecoration(
                         color: Colors.transparent,
@@ -64,15 +67,15 @@ class RubricGradingScale extends StatelessWidget {
                 ],
               ),
               _spacingBetweenRows(),
-              _gradingScaleRow(l),
+              _gradingScaleRow(l: l, low: '90', high: '100', gradeTitle: 'A'),
               _spacingBetweenRows(),
-              _gradingScaleRow(l),
+              _gradingScaleRow(l: l, low: '80', high: '89.9', gradeTitle: 'B'),
               _spacingBetweenRows(),
-              _gradingScaleRow(l),
+              _gradingScaleRow(l: l, low: '70', high: '79.9', gradeTitle: 'C'),
               _spacingBetweenRows(),
-              _gradingScaleRow(l),
+              _gradingScaleRow(l: l, low: '60', high: '69.9', gradeTitle: 'D'),
               _spacingBetweenRows(),
-              _gradingScaleRow(l),
+              _gradingScaleRow(l: l, low: '50', high: '59.9', gradeTitle: 'F'),
             ],
           ),
         ),
@@ -84,24 +87,29 @@ class RubricGradingScale extends StatelessWidget {
 
   SizedBox _spacingBetweenRows() => SizedBox(height: 32);
 
-  Row _gradingScaleRow(AppLocalizations l) {
+  Widget _gradingScaleRow({
+    @required String gradeTitle,
+    @required String low,
+    @required String high,
+    @required AppLocalizations l,
+  }) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        ToggleBtnTitleNull(l.gradingScaleGradeA),
+        BodyGradingScaleInput(gradeTitle),
         Container(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 48, vertical: 16),
-            child: ToggleBtnTitleNull('90'),
+            child: BodyGradingScaleInput(low),
           ),
           decoration: BoxDecoration(
               color: primary, borderRadius: BorderRadius.circular(10)),
         ),
-        ToggleBtnTitleNull(l.gradingScaleTo),
+        BodyGradingScaleInput(l.gradingScaleTo),
         Container(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 48, vertical: 16),
-            child: ToggleBtnTitleNull('100'),
+            child: BodyGradingScaleInput(high),
           ),
           decoration: BoxDecoration(
               color: primary, borderRadius: BorderRadius.circular(10)),
@@ -109,6 +117,13 @@ class RubricGradingScale extends StatelessWidget {
       ],
     );
   }
-
-  //Note to JW: I have extracted widgets to become private methods. However, I want to change out lines 91, 95 and 104 to allow other text in these areas. I wanted to extract widgets now to prevent tons of code / repeats for each row later. Also would like to ensure the Containers are text input fields with a default of each of numerical.
 }
+
+//Also TODO: 
+//6. Fix the warning
+
+//7. Fix how rows / text appear on other devices / testing
+
+//4. You didn’t use any widgets that actually “do” anything. Maybe use a TabBarView instead of building out widgets for the buttons? The buttons can work, but you’ll have to make your own animations and such between the simple and detailed views.
+
+//X. Make small adjustments to inconsistent sizes of containers / padding in inputs.
