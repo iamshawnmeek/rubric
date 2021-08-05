7033b02feaa479db639f4988edff65d411cb152d
iamshawnmeek <shawn@shawnmeek.com>
2021-08-05 17:58:50 -0600

    chore: updating color requirements for button class

--- lib/typography/toggle_button_title_active.dart
+++ lib/typography/toggle_button_title_active.dart
@@ -3,12 +3,10 @@ import 'package:flutter/material.dart';
 class ToggleButtonTitleActive extends StatelessWidget {
   final String data;
   final double fontSize;
-  final Color color;
 
   const ToggleButtonTitleActive(
     this.data, {
     this.fontSize = 21,
-    this.color = Colors.white,
   });
 
   @override
@@ -19,7 +17,6 @@ class ToggleButtonTitleActive extends StatelessWidget {
         fontFamily: 'Avenir-Heavy',
         fontSize: fontSize,
         height: 1.2,
-        color: color,
       ),
     );
   }
